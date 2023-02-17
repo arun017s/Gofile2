542c4d14256bc006b2aa3b2cb4431d3aef827f4c
Arunkumar Shibu <86429222+arun017s@users.noreply.github.com>
2023-02-17 12:12:45 +0530

    mod
    Modified for my comfort 👀👌

--- gofile2/GoFile.py
+++ gofile2/GoFile.py
@@ -10,14 +10,11 @@ from .errors import (InvalidOption, InvalidPath, InvalidToken, JobFailed,
                      ResponseError, is_valid_token)
 
 
-class Async_Gofile:
+class GoFile:
     """
-    ## Gofile Class:
-
-        Base class of Asynchronous Gofile2
-
-    ### Arguments:
-
+    # GoFile Class:
+        - Base class of Asynchronous Gofile2
+    # Arguments:
         - `token` (optional for some functions)- The access token of an account. Can be retrieved from the profile page
     """
 
@@ -38,13 +35,12 @@ class Async_Gofile:
                 error = "Response Status is not ok and reason is unknown"
             raise ResponseError(error)
 
-    async def get_Server(self, pre_session=None):
+    async def getServer(self, pre_session=None):
         """
         ### Get Server Function:
-            Get server of Gofile
-
+            - Get server of Gofile
         ### Arguments:
-            `None`
+            - `None`
         """
         if pre_session:
             server_resp = await pre_session.get(f"{self.api_url}getServer")
@@ -59,44 +55,29 @@ class Async_Gofile:
                 except Exception as e:
                     raise JobFailed(e)
 
-    async def get_Account(self, check_account=False):
+    async def getAccountDetails(self):
         """
         ### Get Account Function:
-
-            Get information about the account
-
+            - Get information about the account
         ### Arguments:
-
-            - `check_account` (optional) - Boolean. Pass True to check if account exists or not. else it'll return all data of account
+            - `None`
         """
         if self.token is None:
             raise InvalidToken()
         async with ClientSession() as session:
             try:
-                get_account_resp = await session.get(url=f"{self.api_url}getAccountDetails?token={self.token}&allDetails=true")
-                get_account_resp = await get_account_resp.json()
-                if check_account is True:
-                    if get_account_resp["status"] == "ok":
-                        return True
-                    elif get_account_resp["status"] == "error-wrongToken":
-                        return False
-                    else:
-                        return await self._api_resp_handler(get_account_resp)
-                else:
-                    return await self._api_resp_handler(get_account_resp)
+                get_account_resp = await session.get(url=f"{self.api_url}getAccountDetails?token={self.token}")
+                get_account_resp = await get_account_resp.json()                
+                return await self._api_resp_handler(get_account_resp)
             except Exception as e:
                 raise JobFailed(e)
 
-    async def upload_folder(self, path: str, folderId: str = "", folder_name: str = "Gofile2", delay: int = 2):
+    async def uploadFolder(self, path: str, folderId: str = "", folder_name: str = "Gofile2", delay: int = 2):
         """
-        NOTE: To use this function, you must have a gofile token
-
+        NOTE: To use this function, you must have a GoFile token
         ### Upload folder Function
-
-            Upload files in the given path to Gofile
-
+            - Upload files in the given path to Gofile
         ### Arguments
-
             - `path` - Path to the folder
             - `folderId` (optional) - The ID of a folder. When using the folderId, you must pass the token
             - `delay` - Time interval between file uploads (in seconds)
@@ -108,57 +89,37 @@ class Async_Gofile:
             i[0], j) for j in i[2]] for i in os.walk(path)] for val in sublist]
         # Get folder id if not passed
         if not folderId:
-            rtfid = (await self.get_Account())["rootFolder"]
-            folderId = (await self.create_folder(rtfid, "Gofile2 - Created in {}".format(strftime("%b %d, %Y %l:%M%p"))))["id"]
+            rtfid = (await self.getAccountDetails())["rootFolder"]
+            folderId = (await self.createFolder(rtfid, "GoFile2- Created in {}".format(strftime("%b %d, %Y %l:%M%p"))))["id"]
         for file in files:
-            udt = await self.upload(file, folderId)
+            udt = await self.uploadFile(file, folderId)
             uploaded.append(udt)
-            await sleep(2)
+            await sleep(delay)
         return uploaded
 
-    async def upload(self, file: str, folderId: str = "", description: str = "", password: str = "", tags: str = "", expire: str = ""):
+    async def uploadFile(self, file: str, folderId: str = ""):
         """
         ### Upload Function:
-
-            Upload a file to Gofile
-
+            - Upload a file to Gofile
         ### Arguments:
-
             - `file` - Path to file that want to be uploaded
-            - `folderId` (optional) - The ID of a folder. When using the folderId, you must pass the token
-
-            [Deprecated options as of 2022-03-25]
-            - `description` (optional) - Description for the uploaded file. Not applicable if you specify a folderId
-            - `password` (optional) - Password for the folder. Not applicable if you specify a folderId
-            - `tags` (optional) - Tags for the folder. If multiple tags, seperate them with comma. Not applicable if you specify a folderId
-            - `expire` (optional) - Expiration date of the folder. Must be in the form of unix timestamp. Not applicable if you specify a folderId
+            - `folderId` (optional) - The ID of a folder. When using the folderId, you must pass the token            
         """
         async with ClientSession() as session:
             # Check time
             if not os.path.isfile(file):
                 raise InvalidPath(f"No such file - {file}")
-            if password and len(password) < 4:
-                raise ValueError("Password Length must be greater than 4")
-
-            server = await self.get_Server(pre_session=session)
+            
+            server = await self.getServer(pre_session=session)
             server = server["server"]
-            token = self.token if self.token else ""
-
+            
             # Making dict
             req_dict = {}
-            if token:
-                req_dict["token"] = token
-            if folderId:
+            if self.token:
+                req_dict["token"] = self.token
+            if bool(folderId):
                 req_dict["folderId"] = folderId
-            if description:
-                req_dict["description"] = description
-            if password:
-                req_dict["password"] = password
-            if tags:
-                req_dict["tags"] = tags
-            if expire:
-                req_dict["expire"] = expire
-
+            
             with open(file, "rb") as go_file_d:
                 req_dict["file"] = go_file_d
                 try:
@@ -171,14 +132,11 @@ class Async_Gofile:
                 except Exception as e:
                     raise JobFailed(e)
 
-    async def create_folder(self, parentFolderId, folderName):
+    async def createFolder(self, parentFolderId, folderName):
         """
         ### Create Folder Function:
-
-            Create a new folder account
-
+            - Create a new folder account
         ### Arguments:
-
             - `parentFolderId` - The parent folder ID
             - `folderName` - The name of the folder that wanted to create
         """
@@ -199,14 +157,11 @@ class Async_Gofile:
             except Exception as e:
                 raise JobFailed(e)
 
-    async def set_folder_option(self, folderId, option, value):
+    async def setFolderOption(self, folderId, option, value):
         """
         ### Set Folder Option Function:
-
-            Set an option on a folder
-
+            - Set an option on a folder
         ### Arguments:
-
             - `folderId` - The ID of the folder
             - `option` - Option that you want to set. Can be "public", "password", "description", "expire" or "tags"
             - `value` - The value of the option to be defined.
@@ -236,14 +191,11 @@ class Async_Gofile:
             except Exception as e:
                 raise JobFailed(e)
 
-    async def get_content(self, contentId):
+    async def getContent(self, contentId):
         """
         ### Get Content Function:
-
-            Get a specific content details
-
+            - Get a specific content details
         ### Arguments:
-
             - `contentId` - The ID of the file or folder
         """
         if self.token is None:
@@ -256,14 +208,11 @@ class Async_Gofile:
             except Exception as e:
                 raise JobFailed(e)
 
-    async def copy_content(self, contentsId, folderIdDest):
+    async def copyContent(self, contentsId, folderIdDest):
         """
         ### Copy Content Function:
-
-            Copy one or multiple contents to another folder
-
+            - Copy one or multiple contents to another folder
         ### Arguments:
-
             - `contentsId` - The ID(s) of the file or folder (Separate each one by comma if there are multiple IDs)
             - `folderIdDest` - Destinatination folder ID
         """
@@ -284,14 +233,11 @@ class Async_Gofile:
             except Exception as e:
                 raise JobFailed(e)
 
-    async def delete_content(self, contentId):
+    async def deleteContent(self, contentId):
         """
         ### Delete Content Function:
-
-            Delete a file or folder
-
+            - Delete a file or folder
         ### Arguments:
-
             - `contentId` - The ID of the file or folder
         """
         if self.token is None:
